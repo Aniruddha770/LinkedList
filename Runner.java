45aefcdbff8c698722b5600cd77cfd68af99a345
Anirudha Patil <48207987+Aniruddha770@users.noreply.github.com>
2021-07-01 16:35:44 +0530

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,16 @@
+/*
+  @author Anirudha
+ */
+//	 *  In this code a simple linked list of integers is created, with  3  int 56,30 and 70.
+
+
+public class Runner {
+    public static void main(String[] args)
+    {
+        LinkedListUC1<Integer> numberList = new LinkedListUC1<Integer>();
+        numberList.add(56);
+        numberList.add(30);
+        numberList.add(70);
+        numberList.show();
+    }
+}
