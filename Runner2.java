98427fee0f014efe04776652eb7bf51562b6683d
Aniruddha770 <patilanirudha38@gmail.com>
2021-07-01 16:24:54 +0530

    Runner 2 added

---
+++ Runner2.java
@@ -0,0 +1,10 @@
+public class Runner2 {
+    public static void main(String[] args)
+    {
+        LinkedListUC2<Integer> numberList = new LinkedListUC2<Integer>();
+        numberList.add(70);
+        numberList.add(30);
+        numberList.add(56);
+        numberList.show();
+    }
+}
