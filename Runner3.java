0b3de38b3feef95dd6034e3cc20d4bc82d8d4728
Aniruddha770 <patilanirudha38@gmail.com>
2021-07-01 16:26:16 +0530

    Runner 3 added

---
+++ Runner3.java
@@ -0,0 +1,9 @@
+public class Runner3 {
+    public static void main(String[] args) {
+        LinkedListUC3<Integer> numberList = new LinkedListUC3<Integer>();
+        numberList.add(56);
+        numberList.add(30);
+        numberList.add(70);
+        numberList.show();
+    }
+}
